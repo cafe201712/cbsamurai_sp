6cb1c248f49d284ec1676a689e60221bf3a86319
cafe201712 <cafe.report.201712@gmail.com>
2018-01-27 20:44:21 +0900

    API 接続先のログ出力を修正

--- src/AppNavigator.vue
+++ src/AppNavigator.vue
@@ -12,7 +12,10 @@ import Login from '@/pages/Login/Login'
 export default {
   name: 'app_navigator',
   beforeCreate () {
-    console.log(process.env)
+    const env = process.env
+    console.log('API_HOST: ' + env.API_HOST)
+    console.log('API_PATH: ' + env.API_PATH)
+    console.log('NODE_ENV: ' + env.NODE_ENV)
     this.$store.commit('navigator/push', Login)
   },
   computed: {
